fa4c8e97d92d1413636eb2e977c51e8ef14e4b73
Nikola Kostadinov <nikolakostadinov@protonmail.com>
2022-08-30 13:26:13 +0300

    hello world

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,14 @@
+import { Navbar, Welcome } from './components'
+
 export default function App () {
 
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className="min-h-screen">
+      <div className="gradient-bg-welcome">
+        <Navbar />
+        <Welcome />
+      </div>
+    </div>
   )
 
 }
---
+++ client/src/components/Welcome.jsx
@@ -0,0 +1,12 @@
+export default function Welcome () {
+    
+    return(
+        <>
+            <h2>Hello,</h2>
+            <h2>and welcome</h2>
+            <h2>to the</h2>
+            <h1>DNX</h1>
+        </>
+    )
+
+}
